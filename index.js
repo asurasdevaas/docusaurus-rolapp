e32a737fc1ec9dab8b87dfd678b42d9478cb979a
asurasdevaas <ewarams@gmail.com>
2023-06-23 08:30:45 -0500

    add site

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  // Read the HTML file
+  fs.readFile('site/build/index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(PORT, () => {
